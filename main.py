350b1fe151fbc7ebd3a61696b861e068811cb292
EdgarElizondo <edgar_7_23@hotmail.com>
2024-07-29 18:14:31 -0600

    Add new info to description in a new column

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image(IMAGEPATH + "photo.png")
+    st.image(IMAGEPATH + "photo.png", width=600)
 with col2:
     st.title("Edgar Elizondo")
     with open(DESCRIPTIONPATH + "myself.txt","r") as file:
         content = file.read()
     content = content.replace("\n", " ")
     st.info (content)
+
+content = """
+Below you can find some of the apps I have builts in Python. Feel free to contact me.
+"""
+st.write(content)
     
